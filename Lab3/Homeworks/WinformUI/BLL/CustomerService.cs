9711e5b75fd321f27d0b8a4d1e7e760d5e8b5ad3
Hài Dón <cathuys69@gmail.com>
2026-02-27 02:01:00 +0700

    adding more

---
+++ Lab3/Homeworks/WinformUI/BLL/CustomerService.cs
@@ -0,0 +1,10 @@
+﻿namespace BLL;
+
+public class CustomerService
+{
+    private readonly CustomerRepository _repo;
+    public CustomerService(CustomerRepository repo) => _repo = repo;
+
+    public List<Customer> GetCustomers() => _repo.GetAll(); // [cite: 304, 306]
+    public void CreateCustomer(Customer c) => _repo.Add(c); // [cite: 294, 297]
+}
---
+++ Lab3/Homeworks/WinformUI/DAL/Exer1DBContext.cs
@@ -0,0 +1,5 @@
+﻿using Microsoft.EntityFrameworkCore;
+public class Exer1DbContext : DbContext {
+    public Exer1DbContext(DbContextOptions<Exer1DbContext> options) : base(options) { }
+    public DbSet<Customer> Customers { get; set; } // [cite: 188]
+}
