381a258ab136982ea534f7ead32a68ff1c542026
Hài Dón <cathuys69@gmail.com>
2026-04-10 23:26:33 +0700

    redo lab 8

--- Lab8/SchoolAppCoreRazor/Pages/Models/ErrorViewModel.cs
+++
@@ -1,7 +0,0 @@
-namespace SchoolAppCoreRazor.Pages.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestID { get; set; }
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestID);
-}
--- Lab8/SchoolAppCoreRazor/Pages/Models/Instructor.cs
+++
@@ -1,9 +0,0 @@
-namespace WebApplication1.Pages.Models{
-    public class Instructor{
-        public int InstructorId { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public DateTime HireDate { get; set; }
-    }
-    
-}
