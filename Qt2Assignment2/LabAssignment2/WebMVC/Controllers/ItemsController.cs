1452d3236f7ed1550ed807b69912ab78110022e5
Hài Dón <cathuys69@gmail.com>
2026-05-23 23:57:51 +0700

    Add Group Assignment 2 (E-Commerce Sales Management System) source code

---
+++ Qt2Assignment2/LabAssignment2/WebMVC/Controllers/ItemsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebMVC_Core.Data;
+using WebMVC_Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebMVC_Core.Controllers
+{
+    public class ItemsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ItemsController(ApplicationDbContext context) => _context = context;
+
+        public async Task<IActionResult> Index() => View(await _context.Items.ToListAsync());
+
+        public IActionResult Create() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Item item)
+        {
+            _context.Add(item);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
